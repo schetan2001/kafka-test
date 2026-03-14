c795102c4d7c056927a580c07c1d4e0a35f1962d
schetan2001 <schetan2001@example.com>
2026-03-14 18:30:14 +0000

    Draft after pg connection fix

--- index.js
+++ index.js
@@ -23,6 +23,9 @@ const pool = new Pool({
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 5000,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 if (!KAFKA_BROKER || !INPUT_TOPIC) {
